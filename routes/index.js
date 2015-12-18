71d0a22682735a0aa80fc38cfef5995ba4a7b5dd
Zackary Corbett <zyber17@gmail.com>
2015-12-18 17:46:30 -0500

    Getting started, wee

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', {title: "Textlr"});
+});
+
+router.post('/', function(req, res, next) {
+	
+});
+
+module.exports = router;
