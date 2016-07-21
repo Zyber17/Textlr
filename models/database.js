ec040e7dd2e019ba59dbd84b0df66b1f7278b5c5
Zackary Corbett <zyber17@gmail.com>
2016-07-20 20:43:24 -0400

    Initial database schema.

---
+++ models/database.js
@@ -0,0 +1,9 @@
+// http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/textlr';
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE texts(id CHAR(10) NOT NULL PRIMARY KEY, markedtext VARCHAR(16000) NOT NULL, nakedtext VARCHAR(16000) NOT NULL, time timestamp default current_timestamp, count SERIAL)');
+query.on('end', function() { client.end(); });
