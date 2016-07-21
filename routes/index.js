6997916b2e68e1b64a8e439bf229f8236cfda94a
Zackary Corbett <zyber17@gmail.com>
2016-07-20 20:42:31 -0400

    Initial post route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+//var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,53 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	
+	// Thanks http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/
+	var pg = require('pg');
+	var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/textlr';
+	var secrets = require('./secrets.js');
+	var crypto = require('crypto');
+	var marked = require('marked');
+	marked.setOptions({
+	  renderer: new marked.Renderer(),
+	  gfm: true,
+	  tables: true,
+	  breaks: false,
+	  pedantic: false,
+	  sanitize: true,
+	  smartLists: true,
+	  smartypants: true
+	});
+
+	var nakedtext = req.body.text;
+	var markedtext = marked(nakedtext);
+
+    var hash = crypto.createHmac('sha512', secrets.secret);
+	var when = (new Date).getTime() - secrets.textlrEpoch;
+    hash.update(when.toString());
+    var id = hash.digest('base64').substring(0,10).replace(/\//g, "=");
+
+	console.log(id);
+	console.log(nakedtext);
+	console.log(markedtext);
+
+	pg.connect(connectionString, function(err, client, done) {
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+		client.query({
+			text:   "INSERT INTO texts(id,markedtext,nakedtext) VALUES ($1, $2, $3)",
+			name:   "add-text",
+			values: [id, nakedtext, markedtext]
+		});
+    });
+
+	res.writeHead(302, {
+		'Location': '/'+id
+	});
+	res.end();
 });
 
 module.exports = router;
